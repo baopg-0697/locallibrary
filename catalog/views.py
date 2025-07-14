d4e3d8f495d39614baf757e49128704236c646f7
GiaBaoPham <quynhanh7890123@gmail.com>
2025-07-14 11:19:18 +0700

    commit part6 Django tutorial

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
 from catalog.constants import LOAN_STATUS
+from django.views import generic
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -25,3 +27,24 @@ def index(request):
     
     #render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    queryset = Book.objects.all()
+    template_name = 'catalog/book_list.html'
+    paginate_by = 1
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some data'] = "This is just some data"
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    context_object_name = 'book'
+   
+    def book_detail_view(request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', 
+                    context={'book': book})
