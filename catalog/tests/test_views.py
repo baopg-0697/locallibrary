15d3671ecc53c8bc5696dab3f3858d9b94faacb4
GiaBaoPham <quynhanh7890123@gmail.com>
2025-07-21 11:37:46 +0700

    commit part 10 Django tutorial
    second commit in part8
    
    first commit part 9
    
    first commit part10
    
    first commit part 10
    
    second commit in part 10
    
    last commit part 10

---
+++ catalog/tests/test_views.py
@@ -0,0 +1,59 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission
+from catalog.models import Author
+
+class AuthorListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Tạo user
+        test_user = User.objects.create_user(username='testuser', password='testpassword123')
+        
+        # Lấy quyền 'view_author' từ database
+        # Tên quyền thường có dạng "Can view <model_name>"
+        permission = Permission.objects.get(name='Can view author')
+        
+        # Gán quyền cho user
+        test_user.user_permissions.add(permission)
+        test_user.save()
+
+        # Tạo 13 tác giả để test
+        number_of_authors = 13
+        for author_id in range(number_of_authors):
+            Author.objects.create(
+                first_name=f'Christian {author_id}',
+                last_name=f'Surname {author_id}',
+            )
+
+    def setUp(self):
+        # Đăng nhập user đã có quyền
+        self.client.login(username='testuser', password='testpassword123')
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/catalog/authors/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/author_list.html')
+
+    def test_pagination_is_ten(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] == True)
+        self.assertEqual(len(response.context['author_list']), 10)
+
+    def test_lists_all_authors(self):
+        # Get second page and confirm it has (exactly) remaining 3 items
+        response = self.client.get(reverse('authors')+'?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] == True)
+        self.assertEqual(len(response.context['author_list']), 3)
